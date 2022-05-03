16918b830f081e7b2a7ce218ea665ac774af87f5
yash1132008 <76216174+yash1132008@users.noreply.github.com>
2022-05-03 16:01:20 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import React,{Component} from 'react';
+import ButtonTabNavigator from './components/ButtonTabNavigator';
+
+export default class App extends Component{
+  render (){
+    return <ButtonTabNavigator/>;
+  }
+}
